6802fb6d3e4fdd8e99d7f84c63b136e7091c8149
Cory-Davis13 <57544514+Cory-Davis13@users.noreply.github.com>
2022-06-13 23:34:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-window.addEventListener("load", (event) => {
-  console.log("page is fully loaded");
-});
-
 document
   .getElementById("btn")
   .addEventListener("click", alert("button clicked"));
